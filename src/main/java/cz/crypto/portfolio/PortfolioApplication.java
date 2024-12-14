3bcc32108f74f8a0d34d8ddda59bab87ead244ef
MartSk2024 <soukenka@post.cz>
2024-12-14 19:44:15 +0100

    Vytvoreni kodu

--- src/main/java/cz/crypto/portfolio/PortfolioApplication.java
+++ src/main/java/cz/crypto/portfolio/PortfolioApplication.java
@@ -17,4 +17,4 @@ public class PortfolioApplication {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
 
-}//konec
+}
--- src/main/java/cz/crypto/portfolio/controller/CryptoController.java
+++ src/main/java/cz/crypto/portfolio/controller/CryptoController.java
@@ -71,4 +71,4 @@ public class CryptoController {
         cryptoService.removeAllCryptos();
     }
 
-}//
+}
